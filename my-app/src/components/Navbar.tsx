53b913bdcc377583602600f3df7afb01feffa6b8
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2024-10-28 17:33:09 -0700

    Updated Navbar component as per figma design.

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,14 +1,22 @@
 import styles from "../styles/Navbar.module.css";
-import SearchBar from "./SearchBar.tsx";
-import default_pfp from "./../../public/images/default_pfp.jpeg";
+import SearchBar from "./SearchBar";
 import Image from "next/image";
+import default_pfp from "./../../public/images/default_pfp.jpeg";
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
-      <h2 className={styles.uniteTitle}>Unite</h2>
-      <SearchBar />
-      <Image className={styles.profilePicture} src={default_pfp} alt="" />
+      <h1 className={styles.uniteTitle}>Unite</h1>
+      <div className={styles.searchContainer}>
+        <SearchBar />
+      </div>
+      <Image
+        src={default_pfp}
+        alt="Profile"
+        className={styles.profileIcon}
+        width={40}
+        height={40}
+      />
     </nav>
   );
 };
