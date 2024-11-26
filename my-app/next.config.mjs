86153c7dfdcc2414830c48ea73059857a08c117e
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2024-11-25 23:17:29 -0800

    update next.config.mjs to allow images from Firebase and Google user content

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "**",
+      },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
         pathname: "**",
       },
     ],
+    unoptimized: true,
   },
 };
 
