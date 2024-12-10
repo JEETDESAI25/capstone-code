da93b205fd429c5c276a385c69f8d54cceaea423
Jeet <113221510+JEETDESAI25@users.noreply.github.com>
2024-12-10 00:27:57 -0800

    Add PopularPosts component and enhance SidePanel navigation</message>
    <message>
    - Introduce PopularPosts component to fetch and display posts sorted by likes from Firebase.
    - Update SidePanel to highlight active links based on the current route.
    - Modify App.module.css to improve section title styling and add posts container layout.
    - Change background color in Campaign styles for better visual consistency.
    - Update CampaignDetails styles to enhance background color and button aesthetics.

--- my-app/src/components/Sidepanel.tsx
+++ my-app/src/components/Sidepanel.tsx
@@ -1,28 +1,53 @@
+"use client";
+
 import Link from "next/link";
 import styles from "../styles/SidePanel.module.css";
+import { usePathname } from "next/navigation";
 
 const SidePanel: React.FC = () => {
+  const pathname = usePathname();
+
   return (
     <div className={styles.sidePanelContainer}>
       <aside className={styles.sidePanel}>
         <ul>
           <li>
-            <Link href="/" className={styles.navLink}>
+            <Link
+              href="/"
+              className={`${styles.navLink} ${
+                pathname === "/" ? styles.active : ""
+              }`}
+            >
               Home
             </Link>
           </li>
           <li>
-            <Link href="/popular" className={styles.navLink}>
+            <Link
+              href="/popular"
+              className={`${styles.navLink} ${
+                pathname === "/popular" ? styles.active : ""
+              }`}
+            >
               Popular
             </Link>
           </li>
           <li>
-            <Link href="/following" className={styles.navLink}>
+            <Link
+              href="/following"
+              className={`${styles.navLink} ${
+                pathname === "/following" ? styles.active : ""
+              }`}
+            >
               Following
             </Link>
           </li>
           <li>
-            <Link href="/campaign" className={styles.navLink}>
+            <Link
+              href="/campaign"
+              className={`${styles.navLink} ${
+                pathname === "/campaign" ? styles.active : ""
+              }`}
+            >
               Campaign
             </Link>
           </li>
